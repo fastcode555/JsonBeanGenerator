719890d9ef924153371a0ca0d7163d66ac0189ad
Barry <87644022+fastcode555@users.noreply.github.com>
2022-10-09 08:44:49 +0700

    feat:谷歌翻译中国区地址被关闭，换成香港的地址

--- src/main/kotlin/com/awesome/utils/HttpApi.kt
+++ src/main/kotlin/com/awesome/utils/HttpApi.kt
@@ -8,7 +8,7 @@ object HttpApi {
 
     fun translate(content: String, translateCode: String): String? {
         val api =
-            "http://translate.google.cn/translate_a/single?client=gtx&dt=t&dj=1&ie=UTF-8&sl=auto&tl=$translateCode&q=$content";
+            "https://translate.google.com.hk/translate_a/single?client=gtx&dt=t&dj=1&ie=UTF-8&sl=auto&tl=$translateCode&q=$content";
         val uri = URI(api, false, "utf-8")
         val response = HttpClient3.doGet(uri.toString())
         val json = response?.toJSON()
