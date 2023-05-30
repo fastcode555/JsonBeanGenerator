e077044b4ee909ec4ed7626c0a382f93dac90f52
Barry <87644022+fastcode555@users.noreply.github.com>
2023-05-30 11:42:12 +0700

    feat:改用正则匹配所有符号

--- src/main/kotlin/com/awesome/utils/JsonHelper.kt
+++ src/main/kotlin/com/awesome/utils/JsonHelper.kt
@@ -1,17 +1,23 @@
 import com.alibaba.fastjson.JSON
 import com.alibaba.fastjson.JSONArray
 import com.alibaba.fastjson.JSONObject
+import com.awesome.utils.regex
 import com.google.common.base.CaseFormat
 
 //dart中的关键词需要转换成普通词
 var KEYS: Array<String> = arrayOf("num", "int", "String", "double", "bool")
 var UPPER_KEYS: Array<String> = arrayOf("List", "Map")
+
+val REGEX_SYMBOL = "[~'`!@#\$%^&*()_\\-+=<>?:\"{}|,./;’\\[\\]·！@#￥%……&*（）——\\-+=\\{\\}|《》？：“”【】；‘’，。、]*";
 var SPECIAL_SYMBOL: Array<String> =
-    arrayOf("-", ",", ".", "=", "'", "?", "!", "！", "？", "，", "。", " ", "\\", "/", "…", "+")
+    arrayOf("-", ",", ".", "=", "'", "?", "!", "！", "？", "，", "。", " ", "\\", "/", "…", "+", ":", "：", "'")
 
 fun String?.toCamel(): String {
     var result = this.clearSymbol()
     if (this == result && !result!!.contains("_")) {
+        //这里增加，强制将单词的第一个字母变为小写
+        val firstWord = result.substring(0, 1)
+        result = firstWord.lowercase() + result.substring(1, result.length)
         return if (KEYS.contains(result)) "${result}x" else result
     }
     result = CaseFormat.LOWER_UNDERSCORE.to(CaseFormat.LOWER_CAMEL, result)
@@ -37,13 +43,13 @@ fun String?.toUpperCamel(): String {
 
 fun String?.clearSymbol(): String? {
     if (this.isNullOrEmpty()) return this
-    var key: String = this
-    for (symbol in SPECIAL_SYMBOL) {
-        if (key.contains(symbol)) {
-            key = key.replace(symbol, "_")
-        }
+    val key: String = this
+    var finalKey: String = key
+    key.regex(REGEX_SYMBOL) {
+        if (it.trim().isEmpty()) return@regex
+        finalKey = finalKey.replace(it, "_")
     }
-    return key
+    return finalKey
 }
 
 fun String?.formatJson(): String? {
